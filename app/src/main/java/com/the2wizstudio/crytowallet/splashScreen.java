60c668f31a341feefb0174c3dbe65351dd420fbb
Forest Defenders Club NFTs <bvkaura@gmail.com>
2022-10-25 16:04:47 +0530

    setup Changes

--- app/src/main/java/com/the2wizstudio/crytowallet/splashScreen.java
+++ app/src/main/java/com/the2wizstudio/crytowallet/splashScreen.java
@@ -2,13 +2,30 @@ package com.the2wizstudio.crytowallet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class splashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.getSupportActionBar().hide();
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(splashScreen.this,Onboarding.class));
+                finish();
+            }
+        },1500);
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+        overridePendingTransition(R.anim.slide_in_left,R.anim.slide_out_left);
     }
 }
